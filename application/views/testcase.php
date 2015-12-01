bc83bf99b851f479e07b29125607e7fedcd92708
fhadsheikh <fhad@websity.ca>
2015-12-01 17:03:19 -0500

    qa

--- application/views/testcase.php
+++ application/views/testcase.php
@@ -1,14 +1,48 @@
 <div class="static-content">
     <div class="page-content">
         <div class="page-heading">            
-            <h1><?php echo $pageTitle; ?></h1>
-                <div class="options">
-                    <div class="btn-toolbar">
-                        <a href="#" class="btn btn-default"><i class="fa fa-fw fa-wrench"></i></a>
-                    </div>
-                </div>
+            <h1>Test Case 1.1.3 | CWEBP - 18</h1><br>
+            <h2>Schedule recurring appointment</h2>
         </div>
         <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="panel panel-plain">
+                        <div class="panel-heading">
+                            <h2>
+                                <ul class="nav nav-tabs"><li class="dropdown pull-right tabdrop hide"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-angle-down"></i> </a><ul class="dropdown-menu"></ul></li>
+                                    <li class="active"><a href="#home" data-toggle="tab">Untested</a></li>
+                                    <li><a href="#tab2" data-toggle="tab">Re-test</a></li>
+                                    <li><a href="#tab2" data-toggle="tab">Bugs</a></li>
+                                </ul>
+                            </h2>
+                        </div>
+                        <div class="panel-body">
+                            test
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-plain">
+                        <div class="panel-heading">
+                            Description
+                        </div>
+                        <div class="panel-body">
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="panel panel-plain">
+                        <div class="panel-heading">
+                            Attachments
+                        </div>
+                        <div class="panel-body">
+                            test
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div> <!-- .container-fluid -->
     </div> <!-- #page-content -->
 </div>
